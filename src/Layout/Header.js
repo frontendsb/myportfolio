a1b6f8e97377a21d1412dfc78db788bb6b5436e0
frontendsb <sutapa20497@gmail.com>
2024-06-30 11:25:58 +0530

    up

---
+++ src/Layout/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <header id="header-section">
+        <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <a className="navbar-brand" href="#">
+            Sutapa Biswas <span>Editor</span>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
