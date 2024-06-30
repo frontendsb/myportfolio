28ed1731c1d1c25c0865666996974c2683e3c6fe
frontendsb <sutapa20497@gmail.com>
2024-06-30 12:52:31 +0530

    up

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -7,29 +7,29 @@ function Footer() {
         <div className="container-fluid">
           <hr></hr>
           <div className="row">
-            <div className="col-xl-3 col-lg-3 col-md-6 col-12">
-              <div>
+            <div className="col-xl-3 col-lg-3 col-md-12 col-12">
+              <div className="pb-3">
                 <h4>Phone</h4>
                 <a href="tel:+(91)9163482544">+(91)9163482544</a>
               </div>
             </div>
-            <div className="col-xl-3 col-lg-3 col-md-6 col-12">
-              <div>
+            <div className="col-xl-3 col-lg-3 col-md-12 col-12">
+              <div className="pb-3">
                 <h4>Email</h4>
                 <a href="mailto:sutapa20497@gmail.com">sutapa20497@gmail.com</a>
               </div>
             </div>
-            <div className="col-xl-3 col-lg-3 col-md-6 col-12">
-              <div>
+            <div className="col-xl-3 col-lg-3 col-md-12 col-12">
+              <div className="pb-3">
                 <h4>Follow Me</h4>
                 <a href="https://www.linkedin.com/in/sutapa-biswas-0ba849250/">
                   <i className="fa-brands fa-linkedin-in"></i>
                 </a>
               </div>
             </div>
-            <div className="col-xl-3 col-lg-3 col-md-6 col-12">
-              <div>
-                <p>© 2035 By Sutapa Biswas.</p>
+            <div className="col-xl-3 col-lg-3 col-md-12 col-12">
+              <div className="pb-3">
+                <p>© 2024 By Sutapa Biswas.</p>
               </div>
             </div>
           </div>
--- src/View/Home/Index.js
+++ src/View/Home/Index.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import './Home.css';
+import my_photo from "../../images/my_photo.jpg";
 
 function index() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <section id="home-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6 col-lg-6 col-md-12 col-12">
+              <div className="my_photo">
+                <div className="photo_box">
+                  <img className="img-fluid" src={my_photo} alt="photo" />
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-12 col-12">
+              <div className="my_about">
+                <div className="cnt_h">
+                  <h1>Hello</h1>
+                </div>
+                <div className="mid_cnt">
+                  <h3>A Bit About Me</h3>
+                  <div className="description">
+                  <p>
+                    I'm a paragraph. Click here to add your own text and edit
+                    me. I’m a great place for you to tell a story and let your
+                    users know a little more about you.
+                  </p>
+                  </div>
+                </div>
+                <div className="link_btn">
+                  <div className="btn_main">
+                    <button>Resume</button>
+                    <button>Projects</button>
+                    <button>Contact</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default index
+export default index;
